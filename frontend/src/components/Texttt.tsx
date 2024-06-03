c679e00e04e1d9c2075f4f711466999142ee28d2
Mausmato <Singeripad2@gmail.com>
2024-06-02 23:18:28 -0400

    feat(frontend): Rotate texting

--- frontend/src/components/Texttt.tsx
+++ frontend/src/components/Texttt.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TextReveal from "../components/textreveal";
 
 const Texttt = () => {
   return (
@@ -6,7 +7,11 @@ const Texttt = () => {
       <h1
         className="text-8xl font-bold hero glitch layers"
         data-text="NeoDev"
-        style={{ fontFamily: "Poppins, sans-serif", position: "relative", zIndex: 10 }}
+        style={{
+          fontFamily: "Poppins, sans-serif",
+          position: "relative",
+          zIndex: 10,
+        }}
       >
         <span>NeoDev</span>
       </h1>
@@ -15,22 +20,17 @@ const Texttt = () => {
 
       <h1
         className="text-6xl font-extrabold text-white pb-3 relative"
-        style={{ fontFamily: "Poppins, sans-serif", position: "relative", zIndex: 10 }}
+        style={{
+          fontFamily: "Poppins, sans-serif",
+          position: "relative",
+          zIndex: 10,
+        }}
       >
-        <span className="relative z-10">League</span>
-        <span className="absolute top-0 left-0 w-full h-full text-white z-0 opacity-50 text-shadow-md blur-sm">
+        <span className="relative  text-center ">
           League
+          <br />
         </span>
       </h1>
-
-      <br />
-
-      <p
-        className="text-2xl font-semibold text-white inline-block bg-clip-text pb-8"
-        style={{ fontFamily: "Poppins, sans-serif", position: "relative", zIndex: 10 }}
-      >
-        The Future of Competitive Programming
-      </p>
     </div>
   );
 };
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-// pages/index.tsx
 import Ripple from "../../components/Ripple";
 import Navbarr from "../../components/Navbar";
 import Texttt from "../../components/Texttt";
@@ -7,22 +6,17 @@ import Logo from "../../assets/logo";
 import { Dock, DockIcon } from "../../components/nDock";
 import "../../style.css";
 import "../../glitch.css";
+import WordRotate from "../../components/textreveal";
 
 const Home: React.FC = () => {
   return (
-    <div className="relative min-h-screen overflow-x-hidden scroll-pb-64">
+    <div className="relative min-h-screen overflow-x-hidden">
       <div className="fixed top-0 left-0 w-full z-50">
-        {/* 
-        <Dock className="bg-gradient-to-b from-transparent via-transparent to-transparent backdrop-blur-md">
-          <DockIcon>ICN1</DockIcon>
-          <DockIcon className="cl">ICN2</DockIcon>
-          <DockIcon>ICN3</DockIcon>
-          <DockIcon>ICN4</DockIcon>
-          <DockIcon>ICN5</DockIcon>
-          {/* Add more stuff if needed 
-        </Dock> 
-        */}
-        <a href="http://localhost:5173" target="_blank" rel="noopener noreferrer">
+        <a
+          href="http://localhost:5173"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <Logo />
         </a>
         <MNav />
@@ -33,6 +27,23 @@ const Home: React.FC = () => {
           <Texttt />
         </div>
         <Ripple />
+      </div>
+
+      <div className="text-center mt-40">
+        <span className="text-4xl md:text-6xl font-bold">The Future of</span>
+        <br />
+        <span className="text-2xl md:text-4xl font-bold">
+          <WordRotate
+            className="font-bold"
+            words={[
+              "Competitive Programming",
+              "Collaboration.",
+              "Innovation.",
+              "Technology",
+              "ETC.",
+            ]}
+          />
+        </span>
       </div>
     </div>
   );
