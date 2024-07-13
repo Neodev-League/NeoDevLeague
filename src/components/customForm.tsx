aa8c5ce28cea2ac580364b92b7c8a3dc3481e4f1
HamzaAmmarPython <68649005+HamzaAmmarPython@users.noreply.github.com>
2024-07-12 20:37:10 -0400

    Put button - Matthew Fix pls

--- src/components/customForm.tsx
+++ src/components/customForm.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { EmailFormFields } from "react-mailchimp-subscribe";
 import { AnimatePresence, motion } from "framer-motion";
-
+import { CheckIcon, ChevronRightIcon } from "lucide-react";
+ 
+import { AnimatedSubscribeButton } from "../../@/components/magicui/animated-subscribe-button.tsx";
 interface CustomFormProps {
   status: string | null;
   message: string | Error | null;
@@ -39,25 +41,27 @@ const CustomForm: React.FC<CustomFormProps> = ({
           transition={{ duration: 0.5 }}
         />
       </div>
+
       <AnimatePresence mode="wait">
         {!isSubscribed && !status && (
-          <motion.button
-            className="relative flex w-48 cursor-pointer items-center justify-center rounded-md bg-green-500 text-white p-3 pr-5 pl-5 shadow-lg transform hover:scale-105 transition-transform duration-300"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 20 }}
-            transition={{ duration: 0.5 }}
-            type="submit"
-          >
-            <motion.span
-              key="initialText"
-              className="relative block font-semibold"
-              initial={{ x: 0 }}
-              exit={{ x: 50, transition: { duration: 0.1 } }}
-            >
-              Subscribe
-            </motion.span>
-          </motion.button>
+          <AnimatedSubscribeButton
+          buttonColor="green"
+          buttonTextColor="#ffffff"
+          subscribeStatus={false}
+          initialText={
+            <span className="group inline-flex items-center">
+              Subscribe{" "}
+              <ChevronRightIcon className="transition-transform duration-300 group-hover:translate-x-1" />
+            </span>
+          }
+          changeText={
+            <span className="group inline-flex items-center">
+              Subscribed{" "}
+              <ChevronRightIcon className="transition-transform duration-300 group-hover:translate-x-1" />
+            </span>
+          }
+          />
+          
         )}
         {isSubscribed && status === "sending" && (
           <div className="p-2 pl-3 pr-3 bg-gray-800 border-e-orange-950 mt-2 rounded-md text-gray-500 font-bold text-lg">
@@ -66,17 +70,17 @@ const CustomForm: React.FC<CustomFormProps> = ({
         )}
         {isSubscribed && status === "error" && (
           <div
-            className="mt-2 text-red-500"
-            dangerouslySetInnerHTML={{ __html: message as string }}
+          className="mt-2 text-red-500"
+          dangerouslySetInnerHTML={{ __html: message as string }}
           />
         )}
         {isSubscribed && status === "success" && (
           <motion.button
-            className="relative flex w-48 items-center justify-center overflow-hidden rounded-md bg-green-500 text-white p-3 shadow-lg"
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 20 }}
-            transition={{ duration: 0.5 }}
+          className="relative flex w-48 items-center justify-center overflow-hidden rounded-md bg-green-500 text-white p-3 shadow-lg"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.5 }}
           >
             <motion.span
               key="changeText"
@@ -84,7 +88,7 @@ const CustomForm: React.FC<CustomFormProps> = ({
               initial={{ y: -50 }}
               animate={{ y: 0 }}
               style={{ color: "green", opacity: 0.6 }}
-            >
+              >
               Subscribed!
             </motion.span>
           </motion.button>
