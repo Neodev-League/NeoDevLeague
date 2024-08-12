e993ba2f241c5fbf53eaf6c7cf9ffae74c5130b2
Hamza <hamza.k.ammar@gmail.com>
2024-08-12 09:41:55 -0400

    Changed newsletter formatting

--- src/components/customForm.tsx
+++ src/components/customForm.tsx
@@ -28,11 +28,11 @@ const CustomForm: React.FC<CustomFormProps> = ({ onValidated }) => {
 
   return (
     <form className="w-full flex flex-col items-center" onSubmit={handleSubmit}>
-      <div className="bg-light2 bg-opacity-60 bg-white/50 rounded-md p-5 w-full max-w-md">
+      <div className="bg-light2 bg-opacity-60 bg-white/50 rounded-md lg:p-5 md:p-2 max-w-md">
         <div className="flex p-3 text-black bg-white/50 rounded-md text-left items-center">
           <div className="mr-3 flex-grow">
-            <h4 className="text-lg font-bold">Sign up for our newsletter!</h4>
-            <p className="text-ms">Stay caught up on all news related to the NeoDev League!</p>
+            <h4 className="lg:text-lg md:text-md font-bold">Sign up for our newsletter!</h4>
+            <p className="lg:text-ms md:text-sm">Stay caught up on all news related to the NeoDev League!</p>
           </div>
           <img src={Logo} className="h-10 w-10" alt="Logo"></img>
         </div>
@@ -43,37 +43,37 @@ const CustomForm: React.FC<CustomFormProps> = ({ onValidated }) => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-            className="p-3 pr-32 border border-gray rounded-md focus:outline-none focus:ring-4 focus:ring-darkerGreen transition-shadow duration-300 bg-opacity-90 w-full"
+            className="lg:p-3 md:p-1 lg:pr-32 md:pr-24 border border-gray rounded-md focus:outline-none focus:ring-4 focus:ring-darkerGreen transition-shadow duration-300 bg-opacity-90 w-full"
             initial={{ opacity: 0, y: 5 }}
-            animate={{ opacity: 1, y: -5 }}
+            animate={{ opacity: 1, y: -6 }}
             transition={{ duration: 1 }}
           />
           <AnimatePresence mode="sync">
             {status === "idle" && (
               <button
                 type="submit"
-                className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center justify-center overflow-hidden rounded-md bg-darkerGreen text-white px-6 py-2 cursor-pointer"
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center justify-center overflow-hidden rounded-md bg-dark3 text-white lg:text-md md:text-ms lg:px-6 md:px-3 lg:py-2 md:py-1 cursor-pointer"
               >
                 Subscribe
               </button>
             )}
             {status === "sending" && (
               <div
-                className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center justify-center overflow-hidden rounded-md bg-blurred text-white px-6 py-2"
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center justify-center overflow-hidden rounded-md bg-blurred text-white lg:px-6 md:px-3 lg:py-2 md:py-1"
               >
                 Sending...
               </div>
             )}
             {status === "error" && (
               <div
-                className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center justify-center overflow-hidden rounded-md bg-red-500 text-white px-6 py-2 text-sm leading-snug text-center cursor-pointer"
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center justify-center overflow-hidden rounded-md bg-red-500 text-white lg:px-6 lg:px-3 lg:py-2 md:py-1 text-sm leading-snug text-center cursor-pointer"
               >
                 Please enter a valid email address.
               </div>
             )}
             {status === "success" && (
               <motion.button
-                className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center justify-center overflow-hidden rounded-md bg-lighterGreen text-white px-6 py-2"
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 flex items-center justify-center overflow-hidden rounded-md bg-lighterGreen text-white lg:px-6 lg:px-3 lg:py-2 md:py-1"
                 initial={{ opacity: 0, y: 0 }}
                 animate={{ opacity: 1, y: -19 }}
                 exit={{ opacity: 0, y: 20 }}
