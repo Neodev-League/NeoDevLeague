f6f6c45036ac5da91854e37bf01ae986c981c5ee
JLi2007 <LiJames9532@gmail.com>
2024-08-25 20:08:00 -0400

    Fix(build): More build errors

--- src/pages/Sponsor/components/vs-text.tsx
+++ src/pages/Sponsor/components/vs-text.tsx
@@ -75,7 +75,7 @@ export function VelocityScroll({
     const x = useTransform(baseX, (v) => `${wrap(-100 / repetitions, 0, v)}%`);
 
     const directionFactor = React.useRef<number>(1);
-    useAnimationFrame((t, delta) => {
+    useAnimationFrame((delta) => {
       let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
 
       if (velocityFactor.get() < 0) {
