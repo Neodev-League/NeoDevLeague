9488600dfbb5fb5489360a6bfc51f801283d9734
Mausmato <Singeripad2@gmail.com>
2024-09-24 23:45:31 -0400

    feat(frontend) dash page #1 complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Sponsor from "./pages/Sponsor/sponsor";
 import Team from "./pages/Team/team";
-
+import DashboardHome from "./pages/Dash/dashhome";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +11,7 @@ const App: React.FC = () => {
       <Route path="/home" element={<Home />} />
       <Route path="/team" element={<Team />} />
       <Route path="/sponsor" element={<Sponsor />} />
+      <Route path="/dashboard" element={<DashboardHome />} />
     </Routes>
   );
 };
