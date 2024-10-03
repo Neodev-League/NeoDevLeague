f4d56ad9124bc893a9e8c8c787e8fbae41f87c83
Mausmato <Singeripad2@gmail.com>
2024-10-02 23:21:03 -0400

    feat(frontend): luh fixes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,8 +11,16 @@ import MacNavbar from "../../components/macNavbar";
 import FAQ from "../../components/Home/FAQ";
 import google from "../../assets/Sponsors/google.png";
 import convictional from "../../assets/Sponsors/convictional.png";
+import bestbuy from "../../assets/Sponsors/bestbuy.svg";
 import googlelcn from "../../assets/Misc/GoogleLcn.png";
-import { FaQuestionCircle, FaMapMarkerAlt, FaUsers } from "react-icons/fa";
+import {
+  FaQuestionCircle,
+  FaMapMarkerAlt,
+  FaUsers,
+  FaCode,
+  FaLaptopCode,
+  FaRocket,
+} from "react-icons/fa";
 
 import "../../style.css";
 import "./glitch.css";
@@ -69,13 +77,30 @@ const SponsorBox: React.FC<{ src: string; alt: string }> = ({ src, alt }) => {
       animate={controls}
       initial="hidden"
       variants={{
-        visible: { opacity: 1, scale: 1, transition: { duration: 0.5} },
+        visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
         hidden: { opacity: 0, scale: 0.8 },
       }}
       whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
       className="bg-white rounded-2xl p-10 shadow-2xl transform transition-transform duration-300 hover:shadow-neon"
     >
       <img src={src} alt={alt} className="h-32 object-contain" />
+    </motion.div>
+  );
+};
+
+const NeoDev3DBox: React.FC<{
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}> = ({ icon, title, description }) => {
+  return (
+    <motion.div
+      className="bg-gradient-to-br from-green-400 to-[#598576] p-6 rounded-lg shadow-lg transform transition-all duration-300 hover:scale-105 hover:rotate-3"
+      whileHover={{ rotateY: 15, rotateX: -15 }}
+    >
+      <div className="text-4xl text-white mb-4">{icon}</div>
+      <h3 className="text-xl font-bold text-white mb-2">{title}</h3>
+      <p className="text-white">{description}</p>
     </motion.div>
   );
 };
@@ -143,7 +168,7 @@ const Home: React.FC = () => {
             visible: { scale: 1, opacity: 1 },
             hidden: { scale: 1.2, opacity: 0 },
           }}
-          transition={{ delay: 1.2, duration: 1, ease: "easeOut" }} // Increased delay by 1 second
+          transition={{ delay: 1.2, duration: 1, ease: "easeOut" }}
         >
           <motion.span
             className="text-4xl md:text-6xl lg:text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#34D399] to-[#76C675]"
@@ -151,7 +176,7 @@ const Home: React.FC = () => {
               visible: { y: 0, opacity: 1 },
               hidden: { y: 50, opacity: 0 },
             }}
-            transition={{ delay: 1.30, duration: 0.5 }} // Increased delay by 1 second
+            transition={{ delay: 1.3, duration: 0.5 }}
           >
             OCT 26th
           </motion.span>
@@ -161,7 +186,7 @@ const Home: React.FC = () => {
               visible: { y: 0, opacity: 1 },
               hidden: { y: 50, opacity: 0 },
             }}
-            transition={{ delay: 1.55, duration: 0.5 }} // Increased delay by 1 second
+            transition={{ delay: 1.55, duration: 0.5 }}
           >
             AT
           </motion.span>
@@ -172,9 +197,9 @@ const Home: React.FC = () => {
           animate="visible"
           variants={{
             visible: { scale: 1, opacity: 1 },
-            hidden: { scale: 1.30, opacity: 0 },
+            hidden: { scale: 1.3, opacity: 0 },
           }}
-          transition={{ delay: 1.67, duration: 0.8, ease: "easeOut" }} // Increased delay by 1 second
+          transition={{ delay: 1.67, duration: 0.8, ease: "easeOut" }}
         >
           <motion.img
             src={google}
@@ -187,7 +212,7 @@ const Home: React.FC = () => {
               duration: 5,
               repeatType: "reverse",
               ease: "easeInOut",
-              delay: 1 // Increased delay by 1 second
+              delay: 1,
             }}
           />
         </motion.div>
@@ -205,7 +230,30 @@ const Home: React.FC = () => {
       </AnimatedSection>
 
       <AnimatedSection className="w-full py-24 md:py-32 px-4 md:px-8 mt-24 md:mt-32 flex flex-col items-center justify-center">
-        <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-white text-shadow mb-8 md:mb-16 text-center">
+        <h2 className="text-5xl md:text-5xl lg:text-6xl font-bold text-white text-shadow mb-12 text-center">
+          The NeoDev Experience
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-6xl">
+          <NeoDev3DBox
+            icon={<FaCode />}
+            title="Prompt Challanges"
+            description="Push your limits with our prompt based programming challenges designed to test your skills."
+          />
+          <NeoDev3DBox
+            icon={<FaLaptopCode />}
+            title="Collaborative Coding"
+            description="Experience the power of teamwork as you solve prompt based problems together in teams of 8-10."
+          />
+          <NeoDev3DBox
+            icon={<FaRocket />}
+            title="Innovation Hub"
+            description="Launch your ideas into reality with our 10+ hour programming period @ the Google building."
+          />
+        </div>
+      </AnimatedSection>
+
+      <AnimatedSection className="w-full py-12 md:py-12 px-4 md:px-8 mt-24 md:mt-32 flex flex-col items-center justify-center">
+        <h1 className="text-4xl md:text-6xl pb-6 lg:text-8xl font-bold text-white text-shadow mb-8 md:mb-16 text-center">
           OUR SPONSORS
         </h1>
         <motion.div
@@ -220,7 +268,7 @@ const Home: React.FC = () => {
                 ease: "easeOut",
                 when: "beforeChildren",
                 staggerChildren: 0.2,
-                delay: 1 // Increased delay by 1 second
+                delay: 1,
               },
             },
             hidden: { opacity: 0, y: 50 },
@@ -229,33 +277,24 @@ const Home: React.FC = () => {
         >
           <motion.h2
             variants={{
-              visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 1 } }, // Increased delay by 1 second
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 0.5, delay: 1.5 },
+              },
               hidden: { opacity: 0, y: 20 },
             }}
-            className="text-3xl md:text-4xl lg:text-6xl font-extrabold mb-6 md:mb-12 text-center"
+            className="text-3xl md:text-4xl lg:text-6xl font-extrabold my-8 text-center text-shadow text-transparent bg-clip-text bg-gradient-to-r from-[rgb(21,237,158)] to-[#007b37]"
           >
-            {["V", "i", "s", "i", "o", "n", "a", "r", "y"].map(
-              (letter, index) => (
-                <motion.span
-                  key={index}
-                  variants={{
-                    visible: {
-                      opacity: 1,
-                      x: 0,
-                      transition: { duration: 0.5, delay: (index + 1) * 0.1 }, // Increased delay by 1 second
-                    },
-                    hidden: { opacity: 0, x: -20 },
-                  }}
-                  className="inline-block text-transparent bg-clip-text bg-gradient-to-r from-dark1 to-dark1"
-                >
-                  {letter}
-                </motion.span>
-              )
-            )}
+            Visionary
           </motion.h2>
           <motion.div
             variants={{
-              visible: { opacity: 1, scale: 1, transition: { duration: 0.5, delay: 1 } }, // Increased delay by 1 second
+              visible: {
+                opacity: 1,
+                scale: 1,
+                transition: { duration: 0.5, delay: 1 },
+              },
               hidden: { opacity: 0, scale: 0.8 },
             }}
             className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-6"
@@ -263,6 +302,33 @@ const Home: React.FC = () => {
             <SponsorBox src={google} alt="Google" />
             <SponsorBox src={convictional} alt="Convictional" />
           </motion.div>
+        </motion.div>
+
+        <motion.h2
+          variants={{
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 1.5 },
+            },
+            hidden: { opacity: 0, y: 20 },
+          }}
+          className="text-3xl md:text-4xl lg:text-6xl font-extrabold my-8 text-center text-shadow text-transparent bg-clip-text bg-gradient-to-r from-[rgb(21,237,158)] to-[#007b37]"
+        >
+          Pathfinder
+        </motion.h2>
+        <motion.div
+          variants={{
+            visible: {
+              opacity: 1,
+              scale: 1,
+              transition: { duration: 0.5, delay: 1.7 },
+            },
+            hidden: { opacity: 0, scale: 0.8 },
+          }}
+          className="flex justify-center"
+        >
+          <SponsorBox src={bestbuy} alt="Best Buy" />
         </motion.div>
       </AnimatedSection>
 
