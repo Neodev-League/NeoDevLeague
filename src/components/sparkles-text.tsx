b1be4507d44eeae68a1971a0dca07ce92ab15362
Mausmato <Singeripad2@gmail.com>
2024-10-14 16:51:10 -0400

    fix

--- src/components/sparkles-text.tsx
+++ src/components/sparkles-text.tsx
@@ -3,7 +3,7 @@
 import { CSSProperties, ReactElement, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
-import { cn } from "../../lib/utils";
+import { cn } from "../lib/utils";
 
 interface Sparkle {
   id: string;
