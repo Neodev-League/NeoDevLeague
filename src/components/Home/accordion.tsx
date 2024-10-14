ce8f065f90faea55ab08b761741e1703f156d2f1
Mausmato <Singeripad2@gmail.com>
2024-10-14 16:45:55 -0400

    fix

--- src/components/Home/accordion.tsx
+++ src/components/Home/accordion.tsx
@@ -2,7 +2,6 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export default function AccordionExpandIcon() {
--- src/components/Home/mobilefooter.tsx
+++ src/components/Home/mobilefooter.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Link from "next/link";
 import { FaInstagram, FaGithub, FaDiscord } from "react-icons/fa";
 
